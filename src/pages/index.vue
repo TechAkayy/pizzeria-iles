5a70213508084dbf5d8030a871c6ee945d9a7d44
Akayy <techakayy@gmail.com>
2023-08-23 12:48:05 +1000

    03 Clone Iles TailwindCSS template

---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<page>
+    title: Home
+</page>
+<template>
+  <main>
+    <HeroSection client:load />
+    <AboutTemplate client:visible />
+    <WalkThrough client:visible />
+  </main>
+</template>
+<style scoped></style>
